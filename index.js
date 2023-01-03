1809363e390e838d2ae96d175e011c811faa2ec9
Severin Buhler <sevi_buehler@hotmail.com>
2023-01-03 14:08:19 +0200

    added basic typescript support

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = {
-  Client: require('./src/Grenache/Client'),
-  Server: require('./src/Grenache/Server'),
-  Worker: require('./src/Worker'),
-  DB: require('./src/DB/DB'),
-  StatusFile: require('./src/StatusFile')
-}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+  Client: require('./Grenache/Client'),
+  Server: require('./Grenache/Server'),
+  Worker: require('./Worker'),
+  DB: require('./DB/DB'),
+  StatusFile: require('./StatusFile')
+}
